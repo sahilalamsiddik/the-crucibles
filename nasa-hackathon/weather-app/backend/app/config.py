7859c8ce61b45e7162174bfe87f442323231b768
shashanka-17 <abscissa190@gmail.com>
2025-10-04 21:07:31 +0530

    Update config.py

--- nasa-hackathon/weather-app/backend/app/config.py
+++ nasa-hackathon/weather-app/backend/app/config.py
@@ -1,2 +1,3 @@
 NASA_API_URL = "https://power.larc.nasa.gov/api/temporal/daily/point"
-NASA_PARAMETERS = ["T2M", "PRECTOT", "WS10M", "RH2M"]  # Temp, Rain, Wind, Humidity
+NASA_PARAMETERS = ["T2M", "PRECTOT", "WS10M", "RH2M,ALLSKY_KT"]  # Temp, Rain, Wind, Humidity,All Sky Insolation Clearness Index
+
